ab8581a2416a412c44ca9b1b26a4a712b0abbbaf
joshitha0818 <joshitha0818@gmail.com>
2025-11-03 17:26:11 +0530

    Implement counter functionality with increment and decrement buttons

--- lib/pages/another_page.dart
+++ lib/pages/another_page.dart
@@ -8,11 +8,62 @@ class AnotherPage extends StatefulWidget {
 }
 
 class _AnotherPageState extends State<AnotherPage> {
+  int count = 100;
+
+  @override
+  void initState() {
+    super.initState();
+    count = 10;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    myMontroller.clear();
+  }
+
+  TextEditingController myMontroller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Counter")),
-      body: Center(child: Text("Count")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Count",
+              style: TextStyle(fontSize: 36, fontWeight: FontWeight.w400),
+            ),
+            Text(
+              "$count",
+              style: TextStyle(fontSize: 36, fontWeight: FontWeight.w800),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    count--;
+                    setState(() {});
+                  },
+                  icon: Icon(Icons.remove),
+                ),
+                IconButton(
+                  onPressed: () {
+                    count++;
+                    setState(() {});
+                  },
+                  icon: Icon(Icons.add),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
+
+  
 }
